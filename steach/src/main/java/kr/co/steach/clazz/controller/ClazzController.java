c7cb1b153bbca5014277312ebbd865048f1ccc1e
HaedongKim <haedoang@gmail.com>
2018-11-23 14:21:17 +0900

    프로젝트 구조 수정2   - 중복 파일 제거   - 경로 변경

--- steach/src/main/java/kr/co/steach/clazz/controller/ClazzController.java
+++ steach/src/main/java/kr/co/steach/clazz/controller/ClazzController.java
@@ -1,5 +1,21 @@
 package kr.co.steach.clazz.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/class")
+@Controller
 public class ClazzController {
 
+	@RequestMapping("/curriculum/curriculum.do")
+	public void curriculum() {
+		
+	}
+	
+	@RequestMapping("/curriculum/seat.do")
+	public void seat() {
+		
+	}
+	
+	
 }
---
+++ steach/src/main/webapp/WEB-INF/jsp/class/curriculum/curriculum.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>curriculum</title>
+	
+</head>
+
+<body>
+	<c:import url="<c:url value="/jsp/header/class/header.html"/>"/>
+</body>
+</html>
