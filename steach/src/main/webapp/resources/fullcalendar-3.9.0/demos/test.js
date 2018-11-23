29b3d8c623f77bfb19d59f4a9c5991f9b1e16646
HaedongKim <haedoang@gmail.com>
2018-11-23 11:01:39 +0900

    프로젝트생성   - 디렉토리 구조 생성   - css/html 파일 추가   - 스프링 환경 설정   - pom.xml 환경 설정

---
+++ steach/src/main/webapp/resources/fullcalendar-3.9.0/demos/test.js
@@ -0,0 +1,31 @@
+var eventArray = [
+    {
+      title: 'ui 작업',
+      start: '2018-11-21',
+      end : '2018-11-24',
+      color:'#00FF00'
+    },
+    {
+        title:'열공기간',
+        start:'2018-11-11',
+        end:'2018-11-11',
+        color:'0000FF'
+    },
+    {   
+        title:'생일',
+        start:'2018-11-30'
+    },
+    {
+        title:'aaa',
+        start:'2018-11-01'
+    },
+    {
+        title:'bbb',
+        start:'2018-11-02'
+    },
+    {
+    title: 'Meeting',
+    start: '2018-11-12T10:30:00',
+    end: '2018-11-12T12:30:00'
+    }
+  ]
